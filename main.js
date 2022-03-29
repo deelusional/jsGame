84e1738fc9bba532905f3b8231f823029cd6b411
dTap <imtapped@protonmail.com>
2022-03-30 00:01:45 +0700

    added functions for game

---
+++ main.js
@@ -0,0 +1,27 @@
+const problemElement = document.querySelector('.problem');
+
+let state = {
+  score: 0,
+  wrongAnswers: 0
+}
+
+function generateNumber(max) { 
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function updateProblem() {
+  state.currentProblem = generateProblem();
+  problemElement.innerHTML = `${state.currentProblem.numberOne} ${state.currentProblem.operator} ${state.currentProblem.numberTwo}`;
+}
+ 
+updateProblem();
+
+function generateProblem() {
+  return {
+    numberOne: generateNumber(10),
+    numberTwo: generateNumber(10),
+    // The generateNumber(2) is for the plus, minus, multiply [0, 1, 2]
+    operator: ['+', '-', 'x'][generateNumber(2)],
+  }
+}
+ 
